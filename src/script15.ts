41b20cbec2b97a1fcc75ceb93dfbf65aabe23e4c
Minskoleopc <minskoleopc@gmail.com>
2023-02-02 22:11:06 +0530

    message

--- src/script15.ts
+++ src/script15.ts
@@ -15,3 +15,20 @@ e1 = new HTMLInputElement()
 
 // <>
 // program 2
+let e2=  <HTMLInputElement>document.querySelector('input[type="text"]')
+e2.value
+// Type assertions 
+function getCount(price:number,discount:number,format:boolean):number|string{
+        let finalPrice=  (price - discount)
+        if(format){
+            return `$${finalPrice}`
+        }
+        else {
+            return finalPrice
+        }
+}
+let a11 = getCount(100,10,true) as String
+let a12 = getCount(100,10,false) as number
+
+
+
